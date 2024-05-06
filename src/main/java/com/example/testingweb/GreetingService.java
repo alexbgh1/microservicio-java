fb8af7f2e8689c5b825c5abf51a83b748ba30752
Alex <alex.garnica@alumnos.uach.cl>
2024-05-05 20:52:47 -0400

    Force problems and run full pipeline

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
+		// Force 'problems' cover Sonar
+		String password = "123456";
+		String password1 = "123456";
+
 		return "Hola, soy Alex Garnica";
 	}
 }
